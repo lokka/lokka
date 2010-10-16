18007f7a22cff759953730c555c0037e275a0ecb
Masaki KOMAGATA <komagata@gmail.com>
2010-10-16 18:19:09 +0900

    changed editor jHtmlArea to CLEditor.

--- public/admin/js/editor.js
+++ public/admin/js/editor.js
@@ -1,16 +1,7 @@
-$(function(){
-  $("textarea.editor").htmlarea({
-    css: '/admin/css/editor.css',
-    toolbar: [
-      ["html"], ["p", "bold", "italic", "underline", "|", "forecolor"],
-      ["link", "unlink", "image"],
-      [{
-        css: "code",
-        text: "Code",
-        action: function(btn) {
-          this.pasteHTML('<pre><code>' + this.getSelectedHTML() + '</code></pre>');
-        }
-      }]
-    ]
+$(function() {
+  $('textarea.editor').cleditor({
+    width: 550,
+    height: 550,
+    controls: 'bold italic underline strikethrough subscript superscript font size style | color highlight removeformat bullets numbering | outdent indent | alignleft center alignright justify | rule image link unlink | pastetext source'
   })
 })
