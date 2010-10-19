4c853e4dab8982af6a831a5043c2ef492d29360a
Takeshi Yabe <tyabe.gm@gmail.com>
2010-10-20 00:53:07 +0800

    fixed ruby-1.9.2 support.

--- lib/lokka.rb
+++ lib/lokka.rb
@@ -65,29 +65,45 @@ end
 
 module Rack
   module Utils
+    alias :escape_org :escape
+    alias :unescape_org :unescape
+
     def escape(s)
-      s.to_s.gsub(/([^ a-zA-Z0-9_.-]+)/n) {
-        '%'+$1.unpack('H2'*bytesize($1)).join('%').upcase
-      }.tr(' ', '+')
-      s.force_encoding(Encoding.default_external)
+      escape_org(s).force_encoding(Encoding.default_external)
     end
     def unescape(s)
-      s.tr('+', ' ').gsub(/((?:%[0-9a-fA-F]{2})+)/n){
-        [$1.delete('%')].pack('H*')
-      }
-      s.force_encoding(Encoding.default_external)
+      unescape_org(s).force_encoding(Encoding.default_external)
     end
   end
 end
 
 module DataMapper
   module Validations
-    class LengthValidator < GenericValidator
+    class LengthValidator
+      alias :value_length_org :value_length
       def value_length(value)
         value.force_encoding(Encoding.default_external)
-        value.to_str.split(//u).size
+        value_length_org(value)
       end
     end
   end
 end
 
+module LuckySneaks
+  module StringExtensions
+    alias :to_url_org :to_url
+    def to_url
+      self.force_encoding(Encoding.default_external)
+    end
+  end
+end
+
+module Tilt
+  class Template
+    alias :render_org :render
+    def render(scope=Object.new, locals={}, &block)
+      output = render_org(scope, locals, &block)
+      output.force_encoding(Encoding.default_external) unless output.nil?
+    end
+  end
+end
