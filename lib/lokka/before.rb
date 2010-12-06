4002b1bd7b0ec92dbe0a4ecefbcd49c517cfd732
Masaki KOMAGATA <komagata@gmail.com>
2010-12-06 22:42:29 +0900

    removed databases auto initializing.

--- lib/lokka/before.rb
+++ lib/lokka/before.rb
@@ -2,23 +2,11 @@ module Lokka
   module Before
     def self.registered(app)
       app.before do
-        begin
-          Site.first
-        rescue DataObjects::SyntaxError
-          Lokka::Database.new.create.setup
-        end
-      end
-
-      app.before do
         @site = Site.first
         @title = @site.title
         @theme = Theme.new(settings.theme)
         @theme_types = []
         session[:locale] = params[:locale] if params[:locale]
-      end
-
-      app.before %r{(?!^/admin/login$)^/admin/.*$} do
-        login_required
       end
 
       app.before %r{(?!^/admin/login$)^/admin/.*$} do
