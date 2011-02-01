57d99518ac4f708826a707ccea5078a5248f625c
Satoshi Ebisawa <e.satoshi@gmail.com>
2011-02-02 01:19:01 +0900

    implement "save as draft" button

--- public/admin/js/editor.js
+++ public/admin/js/editor.js
@@ -4,4 +4,8 @@ $(function() {
     height: 550,
     controls: 'bold italic underline strikethrough subscript superscript font size style | color highlight removeformat bullets numbering | outdent indent | alignleft center alignright justify | rule image link unlink | pastetext source'
   })
+  $('#save_as_draft_button').click(function(event){
+    var published = $('#published');
+    published.val(0);
+  });
 })
