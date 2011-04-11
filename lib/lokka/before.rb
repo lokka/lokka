1d75990ee837bec7af922c7d63c6e0c2773edd13
Koutarou Tanaka <from.kyushu.island@gmail.com>
2011-04-11 11:24:55 +0900

    added r18n for theme

--- lib/lokka/before.rb
+++ lib/lokka/before.rb
@@ -12,6 +12,12 @@ module Lokka
         @title = @site.title
         @theme = Theme.new(settings.theme)
         @theme_types = []
+        if @theme.exist_i18n?
+          R18n.extension_places.reject! do |i18n|
+            i18n.dir.match(@theme.root_dir)
+          end
+          R18n.extension_places << R18n::Loader::YAML.new(@theme.i18n_dir) if @theme.exist_i18n?
+        end
         if params[:locale]
           session[:locale] = params[:locale]
           redirect request.referrer
--- lib/lokka/theme.rb
+++ lib/lokka/theme.rb
@@ -1,5 +1,5 @@
 class Theme
-  attr_reader :name, :root_dir, :root_path, :dir, :path
+  attr_reader :name, :root_dir, :root_path, :dir, :path, :i18n_dir
 
   def initialize(root_dir)
     @root_dir = root_dir
@@ -7,9 +7,14 @@ class Theme
     @name = Site.first.theme
     @dir = "#{@root_dir}/#{@name}"
     @path = "#{@root_path}/#{@name}"
+    @i18n_dir = "#{@dir}/i18n"
   end
 
   def to_path(name)
     "#{@dir}/#{name}"
+  end
+
+  def exist_i18n?
+    File.exist? @i18n_dir
   end
 end
