16d67375508fdb8fff6043d9060bc59f5d924946
Masaki Komagata <komagata@gmail.com>
2011-09-28 21:49:37 +0900

    added basic_auth plugin.

---
+++ public/plugin/lokka-basic_auth/lib/lokka/basic_auth.rb
@@ -0,0 +1,29 @@
+module Lokka
+  module BasicAuth
+    def self.registered(app)
+      app.before do
+        username = Option.basic_auth_username
+        password = Option.basic_auth_password
+        if username and password
+          @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+          unless @auth.provided? && @auth.basic? &&
+          @auth.credentials && @auth.credentials == [username, password]
+            response['WWW-Authenticate'] = %(Basic realm="HTTP Auth")
+            throw(:halt, [401, "Not authorized\n"])
+          end
+        end
+      end
+
+      app.get '/admin/plugins/basic_auth' do
+        haml :'plugin/lokka-basic_auth/views/index', :layout => :'admin/layout'
+      end
+
+      app.post '/admin/plugins/basic_auth' do
+        Option.basic_auth_username = params['basic_auth_username']
+        Option.basic_auth_password = params['basic_auth_password']
+        flash[:notice] = 'Updated.'
+        redirect '/admin/plugins/basic_auth'
+      end
+    end
+  end
+end
