4e58627d8a7d069c7ed55b1ca80863a45e598974
Masaki Komagata <komagata@gmail.com>
2011-10-13 06:40:14 +0900

    ruby1.9.2 dependented code move to ruby19.rb

--- lib/lokka.rb
+++ lib/lokka.rb
@@ -24,6 +24,7 @@ require 'sass'
 require 'slim'
 require 'builder'
 require 'nokogiri'
+require 'ruby19' if RUBY_VERSION >= '1.9'
 
 autoload :Theme, 'lokka/theme'
 autoload :User, 'lokka/user'
@@ -102,76 +103,3 @@ module Lokka
     end
   end
 end
-
-unless String.public_method_defined?(:force_encoding)
-  class String
-    def force_encoding(encoding)
-      self
-    end
-  end
-end
-
-unless String.public_method_defined?(:encoding)
-  class String
-    def encoding
-      self
-    end
-  end
-end
-
-unless defined? Encoding
-  class Encoding
-    UTF_8 = nil
-    BINARY = nil
-    def self.default_external
-      nil
-    end
-  end
-end
-
-module Rack
-  module Utils
-    alias :escape_org :escape
-    alias :unescape_org :unescape
-
-    def escape(s)
-      escape_org(s).force_encoding(Encoding.default_external)
-    end
-    def unescape(s)
-      unescape_org(s).force_encoding(Encoding.default_external)
-    end
-  end
-end
-
-module DataMapper
-  module Validations
-    class LengthValidator
-      alias :value_length_org :value_length
-      def value_length(value)
-        value.force_encoding(Encoding.default_external)
-        value_length_org(value)
-      end
-    end
-  end
-end
-
-module LuckySneaks
-  module StringExtensions
-    alias :to_url_org :to_url
-    def to_url
-      self.force_encoding(Encoding.default_external)
-    end
-  end
-end
-
-module Tilt
-  class Template
-    alias :render_org :render
-    def render(scope=Object.new, locals={}, &block)
-      output = render_org(scope, locals, &block)
-      output.force_encoding(Encoding.default_external) unless output.nil?
-    end
-  end
-end
-
-Slim::Engine.set_default_options :pretty => true
