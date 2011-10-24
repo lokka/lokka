1ada74966acadd9238728972bc8762d09f985f30
Masaki Komagata <komagata@gmail.com>
2011-10-24 19:39:22 +0900

    fixed #104

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ created_at = updated_at = "2011-01-09T05:39:08+09:00"
 
 Site.create!(:title => "Test Site",
              :description => "description...",
-             :dashboard => %q(<p>Welcome to Lokka!</p><p>To post a new article, choose ""<a href=""/admin/posts/new"">New</a>"" under ""Posts"" on the menu to the left. To change the title of the site, choose ""Settings"" on the menu to the left. (The words displayed here can be changed anytime through the ""<a href=""/admin/site/edit"">Settings</a>"" screen.)</p>),
+             :dashboard => %q(<p>Welcome to Lokka!</p><p>To post a new article, choose "<a href=""/admin/posts/new"">New</a>" under "Posts" on the menu to the left. To change the title of the site, choose "Settings" on the menu to the left. (The words displayed here can be changed anytime through the "<a href=""/admin/site/edit"">Settings</a>" screen.)</p>),
              :theme => "jarvi",
              :created_at => created_at,
              :updated_at => updated_at) && puts("site was successfully created") if Site.all.blank?
