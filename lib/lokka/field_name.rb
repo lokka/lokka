83310e7ba8c72a9ab3655084af857ad1d3a61651
Junya Ishihara <junya@champierre.com>
2011-11-30 20:53:41 +0900

    Added validate_if_entry_respond_to to FieldName validations.

--- lib/lokka/field_name.rb
+++ lib/lokka/field_name.rb
@@ -7,6 +7,17 @@ class FieldName
   property :updated_at, DateTime
 
   validates_uniqueness_of :name
+  validates_presence_of :name
+  validates_with_method :validate_if_entry_respond_to
+
+  def validate_if_entry_respond_to
+    entry = Entry.new
+    if entry.respond_to?(self.name, true)
+      [false, "'#{self.name}' cannot be used because Entry has a method of the same name"]
+    else
+      return true
+    end
+  end
 end
 
 def FieldName(id)
