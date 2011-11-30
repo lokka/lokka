c04809fcbfcd4307a2515faba33dcce28fb0d54b
tkawa <tkawa@4bit.net>
2011-11-30 21:47:00 +0900

    CLEditor を jWysiwyg に変えてる途中

--- public/admin/js/editor.js
+++ public/admin/js/editor.js
@@ -1,42 +1,44 @@
 $(function() {
-  var cleditor;
+  var wysiwyg;
   var textarea = $('textarea.editor').clone();
   var plainTextareaMode;
 
-  var switchTextareaAndCleditor = (function() {
+  var switchTextareaAndWysiwyg = (function() {
     var name = $('select[name$="[markup]"] option:selected').val();
     name = name == '' ? 'html' : name
     var html;
     if (name == 'html') {
-      // enable cleditor
+      // enable wysiwyg
       if (plainTextareaMode) {
         textarea = $('textarea.editor').clone();
-        translateToCleditor($('textarea.editor'));
+        translateToWysiwyg($('textarea.editor'));
         plainTextareaMode = false;
       }
     } else {
       // enable textarea
       if (!plainTextareaMode) {
-        html = cleditor[0].doc.body.innerHTML;
+        console.log(wysiwyg);
+        html = wysiwyg[0].doc.body.innerHTML;
         textarea[0].innerHTML = html;
-        $('.cleditorMain').remove();
+        $('div.wysiwyg').remove();
         $('#editor').prepend(textarea);
         plainTextareaMode = true;
       }
     }
   });
 
-  var translateToCleditor = (function(jQueryObj) {
-    cleditor = jQueryObj.cleditor({
-      width: 550,
-      height: 550,
-      controls: 'bold italic underline strikethrough subscript superscript font size style | color highlight removeformat bullets numbering | outdent indent | alignleft center alignright justify | rule image link unlink | pastetext source'
-    });
+  var translateToWysiwyg = (function(jQueryObj) {
+    wysiwyg = jQueryObj.wysiwyg();
+//    wysiwyg = jQueryObj.wysiwyg({
+//      width: 550,
+//      height: 550,
+//      controls: 'bold italic underline strikethrough subscript superscript font size style | color highlight removeformat bullets numbering | outdent indent | alignleft center alignright justify | rule image link unlink | pastetext source'
+//    });
   });
 
-  $('select[name$="[markup]"]').change(switchTextareaAndCleditor);
+  $('select[name$="[markup]"]').change(switchTextareaAndWysiwyg);
 
   plainTextareaMode = true;
 
-  switchTextareaAndCleditor();
+  switchTextareaAndWysiwyg();
 });
