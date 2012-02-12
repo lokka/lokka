b548e96183c85e91e3ae20e4cc27b0b4a5c46c66
tomykaira <tomykaira@gmail.com>
2012-02-12 19:00:20 +0900

    Add POST/PUT/DELETE spec for /admin/categories

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -419,11 +419,11 @@ describe "App" do
       end
     end
 
-    context 'with a category' do
+    context '/admin/categories' do
       before { @category = Factory(:category) }
       after { Category.destroy }
 
-      context '/admin/categories' do
+      context 'GET /admin/categories' do
         it 'should show index' do
           get '/admin/categories'
           last_response.should be_ok
@@ -438,11 +438,38 @@ describe "App" do
         end
       end
 
+      context 'POST /admin/categories' do
+        it 'should create a new category' do
+          sample = { :title => 'Created Category',
+            :description => 'This is created in spec',
+            :slug => 'created-category' }
+          post '/admin/categories', { :category => sample }
+          last_response.should be_redirect
+          Category('created-category').should_not be_nil
+        end
+      end
+
       context '/admin/categories/:id/edit' do
         it 'should show form for edit categories' do
           get "/admin/categories/#{@category.id}/edit"
           last_response.should be_ok
           last_response.body.should match('<form')
+        end
+      end
+
+      context 'PUT /admin/categories/:id' do
+        it 'should update the category\'s description' do
+          put "/admin/categories/#{@category.id}", { :category => { :description => 'updated' } }
+          last_response.should be_redirect
+          Category(@category.id).description.should == 'updated'
+        end
+      end
+
+      context 'DELETE /admin/categories/:id' do
+        it 'should delete the category' do
+          delete "/admin/categories/#{@category.id}"
+          last_response.should be_redirect
+          Category(@category.id).should be_nil
         end
       end
     end
