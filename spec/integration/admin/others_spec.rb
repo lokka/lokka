0ef798877ee6b6d3017e11cbb01cdc7a198c3923
tomykaira <tomykaira@gmail.com>
2012-02-13 21:01:59 +0900

    Require spec_helper with absolute path using File.expand_path
    In Ruby 1.8, `require` recognizes a file with different relative path as different files.
    This made spec_helper required twice, then defined shared_context twice, which threw error.

--- spec/integration/admin/others_spec.rb
+++ spec/integration/admin/others_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/spec_helper'
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe 'access admin page' do
   include_context 'admin login'
