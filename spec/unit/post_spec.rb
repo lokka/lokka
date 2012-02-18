ff3f31ed0609ae36d496acf7a6dd325659a2c925
Naoto Takai <takai@recompile.net>
2012-02-18 12:15:03 +0900

    Modernize specs

--- spec/unit/post_spec.rb
+++ spec/unit/post_spec.rb
@@ -1,13 +1,8 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Post do
-  after do
-    Post.destroy
-    User.destroy # User is generated for association
-  end
-
   context 'with slug' do
-    subject { Factory(:post_with_slug) }
+    subject { build :post_with_slug }
 
     its(:link) { should eq('/welcome-lokka') }
 
@@ -31,7 +26,7 @@ describe Post do
   end
 
   context "with id 1" do
-    subject { Factory(:post, :id => 1) }
+    subject { build :post, :id => 1 }
     its(:edit_link) { should eq('/admin/posts/1/edit') }
   end
 
@@ -45,40 +40,38 @@ describe Post do
     end
 
     context 'default' do
-      let(:post) { Factory(:post) }
+      let(:post) { build :post }
       it { post.body.should == post.raw_body }
     end
   end
 
   context "previous or next" do
-    before do
-      @before = Factory(:xmas_post)
-      @after = Factory(:newyear_post)
-    end
+    let!(:before) { create :xmas_post }
+    let!(:after)  { create :newyear_post }
 
     it "should return previous page instance" do
-      @after.prev.should == @before
-      @after.prev.created_at.should < @after.created_at
+      after.prev.should == before
+      after.prev.created_at.should < after.created_at
     end
 
     it "should return next page instance" do
-      @before.next.should == @after
-      @before.next.created_at.should > @before.created_at
+      before.next.should == after
+      before.next.created_at.should > before.created_at
     end
 
     describe "the latest article" do
-      subject { @after }
+      subject { after }
       its(:next) { should be_nil }
     end
 
     describe "the first article" do
-      subject { @before }
+      subject { before }
       its(:prev) { should be_nil }
     end
   end
 
   describe '.first' do
-    before { Factory(:post) }
-    it { lambda { Post.first }.should_not raise_error }
+    before { build :post }
+    it { expect { Post.first }.not_to raise_error }
   end
 end
--- spec/unit/tag_spec.rb
+++ spec/unit/tag_spec.rb
@@ -1,17 +1,14 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe 'Tag' do
-  after do
-    Tag.destroy
-    Tagging.destroy
-  end
-
   context "with name lokka" do
-    before { @tag = Factory(:tag, :name => 'lokka') }
-    subject { @tag }
+    let!(:tag) { create :tag, :name => 'lokka' }
+    subject { tag }
+
     its(:link) { should == '/tags/lokka/' }
+
     it 'Tag(name) should return the instance' do
-      Tag('lokka').should eql(@tag)
+      Tag('lokka').should eql(tag)
     end
   end
 end
