0613adb4843a449132f1450f4f5fb18e7c71b4f0
Masaki Komagata <komagata@gmail.com>
2013-02-28 20:47:04 +0900

    Remove ruby 1.8.7 support

--- config.ru
+++ config.ru
@@ -1,3 +1,2 @@
-Encoding.default_external = 'utf-8' if defined?(Encoding) && Encoding.respond_to?('default_external')
 require './init'
 run Lokka::App
--- init.rb
+++ init.rb
@@ -1,14 +1,7 @@
 $:.unshift File.dirname(__FILE__)
 $:.unshift File.join(File.dirname(__FILE__), 'lib')
 
-if ENV['LOKKA_BUNDLE']
-  ENV['GEM_HOME'] = File.expand_path(File.dirname(__FILE__), 'vendor/gem')
-  $:.unshift File.expand_path(File.dirname(__FILE__), 'vendor/rubygems/lib')
-  require 'rubygems'
-else
-  require 'rubygems'
-  require 'bundler'
-  Bundler.setup
-end
-
+require 'rubygems'
+require 'bundler'
+Bundler.setup
 require 'lokka'
