d8e86e591930700909e57ceae63215c8f4d6835f
Hiroshi Yoshida <hrysd22@gmail.com>
2013-05-17 19:37:12 +0900

    Use Sinatra::Namespace.
    and move helper function.

--- lib/lokka/app.rb
+++ lib/lokka/app.rb
@@ -23,6 +23,7 @@ module Lokka
       set :admin_per_page, 200
       set :default_locale, 'en'
       set :haml, :ugly => false, :attr_wrapper => '"'
+      set :session_secret, 'development' if development?
       supported_stylesheet_templates.each do |style|
         set style, :style => :expanded
       end
@@ -31,12 +32,13 @@ module Lokka
       helpers Lokka::PermalinkHelper
       helpers Lokka::RenderHelper
       helpers Kaminari::Helpers::SinatraHelpers
-      use Rack::Session::Cookie,
-        :expire_after => 60 * 60 * 24 * 12,
-        :secret => SecureRandom.hex(30)
-      set :session_secret, 'development' if development?
+      use Rack::Session::Cookie, {
+        expire_after: 60 * 60 * 24 * 12,
+        secret: SecureRandom.hex(30)
+      }
       register Sinatra::Flash
       register Padrino::Helpers
+      register Sinatra::Namespace
       Lokka.load_plugin(self)
       Lokka::Database.connect
     end
--- lib/lokka/app/admin/tags.rb
+++ lib/lokka/app/admin/tags.rb
@@ -1,32 +1,36 @@
 module Lokka
   class App
-    get '/admin/tags' do
-      @tags = Tag.
-        page(params[:page]).
-        per(settings.admin_per_page)
-      haml :'admin/tags/index', layout: :'admin/layout'
-    end
+    namespace '/admin' do
+      namespace '/tags' do
+        get do
+          @tags = Tag.
+            page(params[:page]).
+            per(settings.admin_per_page)
+          haml :'admin/tags/index', layout: :'admin/layout'
+        end
 
-    get '/admin/tags/:id/edit' do |id|
-      @tag = Tag.where(id: id).first or raise Sinatra::NotFound
-      haml :'admin/tags/edit', layout: :'admin/layout'
-    end
+        get '/:id/edit' do |id|
+          @tag = Tag.where(id: id).first or raise Sinatra::NotFound
+          haml :'admin/tags/edit', layout: :'admin/layout'
+        end
 
-    put '/admin/tags/:id' do |id|
-      @tag = Tag.where(id: id).first or raise Sinatra::NotFound
-      if @tag.update_attributes(params[:tag])
-        flash[:notice] = t('tag_was_successfully_updated')
-        redirect to("/admin/tags/#{@tag.id}/edit")
-      else
-        haml :'admin/tags/edit', layout: :'admin/layout'
+        put '/:id' do |id|
+          @tag = Tag.where(id: id).first or raise Sinatra::NotFound
+          if @tag.update_attributes(params[:tag])
+            flash[:notice] = t('tag_was_successfully_updated')
+            redirect to("/admin/tags/#{@tag.id}/edit")
+          else
+            haml :'admin/tags/edit', layout: :'admin/layout'
+          end
+        end
+
+        delete '/:id' do |id|
+          tag = Tag.where(id: id).first or raise Sinatra::NotFound
+          tag.destroy
+          flash[:notice] = t('tag_was_successfully_deleted')
+          redirect to('/admin/tags')
+        end
       end
-    end
-
-    delete '/admin/tags/:id' do |id|
-      tag = Tag.where(id: id).first or raise Sinatra::NotFound
-      tag.destroy
-      flash[:notice] = t('tag_was_successfully_deleted')
-      redirect to('/admin/tags')
     end
   end
 end
