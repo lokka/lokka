e15fa646f3de5deab49ac7b5441e48f409d11fe4
jiikko <n905i.1214@gmail.com>
2014-03-23 13:41:12 +0900

    Change limit of password_dijest in users

--- db/migration/20130108000009_create_users.rb
+++ db/migration/20130108000009_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration
       t.integer :id
       t.string  :name,             limit: 40, unique: true
       t.string  :email,            limit: 40, unique: true
-      t.string  :password_digest,  limit: 50
+      t.string  :password_digest,  limit: 60
       t.integer :permission_level, default: 1
 
       t.timestamps
