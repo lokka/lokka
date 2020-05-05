eada53add94f508e2263c7f24c67d46ae81e5e04
Hitoshi Nakashima <morygonzalez@gmail.com>
2020-05-05 23:07:27 +0900

    Add password_digest column if users table already exists

--- db/migration/20130108000009_create_users.rb
+++ db/migration/20130108000009_create_users.rb
@@ -2,15 +2,19 @@
 
 class CreateUsers < ActiveRecord::Migration[4.2]
   def change
-    return if table_exists? :users
+    if table_exists? :users
+      change_table :users do |t|
+        t.string  :password_digest,  limit: 60
+      end
+    else
+      create_table :users do |t|
+        t.string  :name,             limit: 40, unique: true
+        t.string  :email,            limit: 40, unique: true
+        t.string  :password_digest,  limit: 60
+        t.integer :permission_level, default: 1
 
-    create_table :users do |t|
-      t.string  :name,             limit: 40, unique: true
-      t.string  :email,            limit: 40, unique: true
-      t.string  :password_digest,  limit: 60
-      t.integer :permission_level, default: 1
-
-      t.timestamps
+        t.timestamps
+      end
     end
   end
 end
